6ef74e81362e026dc71aa2927965bc4b49698183
Derek Jones <derekj@xpedia.co.za>
2018-10-11 11:16:44 +0200

    Controler name changes

--- PAS/app/post/post.js
+++ PAS/app/post/post.js
@@ -1,14 +1,14 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.post', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/post.html',
-    controller: 'View2Ctrl'
+  $routeProvider.when('/post', {
+    templateUrl: 'post/post.html',
+    controller: 'PostCtrl'
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('PostCtrl', [function() {
 
 }]);
